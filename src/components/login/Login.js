2423a351d15e6ffdfee1fe6ea34aa4833d9706f5
Emilson D. Quintero P <e.quinterop@uniandes.edu.co>
2023-02-08 10:05:12 -0500

    localization

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -3,20 +3,32 @@ import axios from "axios";
 import './Login.css';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useTranslation } from "react-i18next";
 
 function Login(props) {
     const [loginForm, setloginForm] = useState({
         email: "",
         password: ""
     })
-    const [error, setError] = useState();
+    const [errorCred, setErrorCred] = useState();
+    const [errorCaps, setErrorCaps] = useState();
+    const [isCapsLockOn, setIsCapsLockOn] = useState(false);
     const navigate = useNavigate()
     const { loggedIn } = props
+    const { t } = useTranslation();
     useEffect(() => {
         if (loggedIn) {
             navigate('/')
         }
     })
+
+    const checkCapsLock = (event) => {
+        if (event.getModifierState('CapsLock')) {
+            setIsCapsLockOn(true);
+        } else {
+            setIsCapsLockOn(false);
+        }
+    };
 
     function logMeIn(event) {
         event.preventDefault()
@@ -28,17 +40,17 @@ function Login(props) {
                 password: loginForm.password
             }
         })
-        .then((response) => {
-            props.setToken(response.data.access_token);
-            localStorage.setItem("userName", response.data.username.username)
-            navigate('/')
-        }).catch((error) => {
-            setloginForm(({
-                email: "",
-                password: ""
-            }))
-            setError('Invalid Username or Password')
-        })
+            .then((response) => {
+                props.setToken(response.data.access_token);
+                localStorage.setItem("userName", response.data.username.username)
+                navigate('/')
+            }).catch((error) => {
+                setloginForm(({
+                    email: "",
+                    password: ""
+                }))
+                setErrorCred('Invalid Username or Password')
+            })
     }
 
     function handleChange(event) {
@@ -49,18 +61,6 @@ function Login(props) {
         )
     }
 
-    document.addEventListener('DOMContentLoaded', () => {
-        const miInput = document.getElementById('password');
-
-        miInput.addEventListener('keyup', function (event) {
-            if (event.getModifierState('CapsLock')) {
-                setError('Alert: Caps Lock key is on');
-            } else {
-                setError('');
-            }
-        });
-    });
-    
     const goToSignup = (event) => {
         event.preventDefault()
         navigate('/signup')
@@ -75,19 +75,22 @@ function Login(props) {
                     type="email"
                     text={loginForm.email}
                     name="email"
-                    placeholder="Email"
+                    placeholder={t("email")}
                     value={loginForm.email} />
                 <input id="password" onChange={handleChange}
                     data-testid="password"
                     type="password"
                     text={loginForm.password}
                     name="password"
-                    placeholder="Password"
+                    placeholder={t("password")}
                     value={loginForm.password} />
-                <div><label className='label_error'>{error}</label></div>
-                <button id="goToSignup" className="goToSignup" onClick={goToSignup}>Don't have an account?</button>
-                <button id="submitbtn1" onClick={logMeIn}>Submit</button>
-                <a href='#'>Forgot password?</a>
+                <div><label className='label_error'>{errorCred}</label></div>
+                {isCapsLockOn && (
+                    <p className='label_error'>{t('errorBMayus')}</p>
+                )}                
+                <button id="goToSignup" className="goToSignup" onClick={goToSignup}>{t('signin')}</button>
+                <button id="submitbtn1" onClick={logMeIn}>{t('submit')}</button>
+                <a href='#'>{t("forgotpwd")}</a>
             </form>
         </Fragment>
     );
