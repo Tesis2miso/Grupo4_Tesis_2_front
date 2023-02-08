f039260ce4c251c6ff7c78819baa1be7f7c23bbc
abaron10 <af.baron10@uniandes.edu.co>
2023-02-08 08:39:58 -0500

    mods in login page

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,92 +1,115 @@
-import { useState, Fragment } from 'react';
+import { useState, Fragment } from "react";
 import axios from "axios";
-import './Login.css';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import "./Login.css";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login(props) {
-    const [loginForm, setloginForm] = useState({
-        email: "",
-        password: ""
-    })
-    const [error, setError] = useState();
-    const navigate = useNavigate()
-    const { loggedIn } = props
-    useEffect(() => {
-        if (loggedIn) {
-            navigate('/')
-        }
-    })
-
-    function logMeIn(event) {
-        event.preventDefault()
-        axios({
-            method: "POST",
-            url: `${process.env.REACT_APP_BASE_PATH}/specialist/login`,
-            data: {
-                email: loginForm.email,
-                password: loginForm.password
-            }
-        })
-        .then((response) => {
-            props.setToken(response.data.access_token);
-            navigate('/')
-        }).catch((error) => {
-            setloginForm(({
-                email: "",
-                password: ""
-            }))
-            setError('Invalid Username or Password')
-        })
+  const [loginForm, setloginForm] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState();
+  const navigate = useNavigate();
+  const { loggedIn } = props;
+  useEffect(() => {
+    if (loggedIn) {
+      navigate("/");
     }
+  });
 
-    function handleChange(event) {
-        const { value, name } = event.target
-        setloginForm(prevNote => ({
-            ...prevNote, [name]: value
-        })
-        )
-    }
-
-    document.addEventListener('DOMContentLoaded', () => {
-        const miInput = document.getElementById('password');
-
-        miInput.addEventListener('keyup', function (event) {
-            if (event.getModifierState('CapsLock')) {
-                alert("Bloq Mayús esta activado");
-            }
+  function logMeIn(event) {
+    event.preventDefault();
+    axios({
+      method: "POST",
+      url: `${process.env.REACT_APP_BASE_PATH}/specialist/login`,
+      data: {
+        email: loginForm.email,
+        password: loginForm.password,
+      },
+    })
+      .then((response) => {
+        props.setToken(response.data.access_token);
+        navigate("/");
+      })
+      .catch((error) => {
+        setloginForm({
+          email: "",
+          password: "",
         });
-    });
-    
-    const goToSignup = (event) => {
-        event.preventDefault()
-        navigate('/signup')
-    }
+        setError("Invalid Username or Password");
+      });
+  }
 
-    return (
-        <Fragment>
-            <form className="login">
-                <h1>DermoApp</h1>
-                <br></br>
-                <input id="username" onChange={handleChange}
-                    type="email"
-                    text={loginForm.email}
-                    name="email"
-                    placeholder="Email"
-                    value={loginForm.email} />
-                <input id="password" onChange={handleChange}
-                    data-testid="password"
-                    type="password"
-                    text={loginForm.password}
-                    name="password"
-                    placeholder="Password"
-                    value={loginForm.password} />
-                <label className='label_error'>{error}</label>
-                <button id="goToSignup" className="goToSignup" onClick={goToSignup}>¿No tienes cuenta? Registrarme</button>
-                <button id="submitbtn1" onClick={logMeIn}>Submit</button>
-            </form>
-        </Fragment>
-    );
+  function handleChange(event) {
+    const { value, name } = event.target;
+    setloginForm((prevNote) => ({
+      ...prevNote,
+      [name]: value,
+    }));
+  }
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const miInput = document.getElementById("password");
+
+    miInput.addEventListener("keyup", function (event) {
+      if (event.getModifierState("CapsLock")) {
+        alert("Bloq Mayús esta activado");
+      }
+    });
+  });
+
+  const goToSignup = (event) => {
+    event.preventDefault();
+    navigate("/signup");
+  };
+
+  return (
+    <Fragment>
+      <div>
+        <form className="login">
+          <div id="shadow">
+            <h1>DermoApp</h1>
+            <br></br>
+            <div className="login-form">
+              <input
+                id="username"
+                onChange={handleChange}
+                type="email"
+                text={loginForm.email}
+                name="email"
+                className="form-control"
+                placeholder="Email"
+                value={loginForm.email}
+                style={{ width: "90%", display: "grid" }}
+              />
+              <br />
+
+              <input
+                id="password"
+                onChange={handleChange}
+                data-testid="password"
+                className="form-control"
+                type="password"
+                text={loginForm.password}
+                name="password"
+                placeholder="Password"
+                style={{ width: "90%", display: "grid"}}
+                value={loginForm.password}
+              />
+            </div>
+
+            <label className="label_error">{error}</label>
+            <button id="goToSignup" className="goToSignup" onClick={goToSignup}>
+              ¿No tienes cuenta? Registrarme
+            </button>
+            <br />
+            <button type="button" style={{ width: "70%"}} class="btn btn-success" id="submitbtn1" onClick={logMeIn}>Submit</button>
+          </div>
+        </form>
+      </div>
+    </Fragment>
+  );
 }
 
 export default Login;
